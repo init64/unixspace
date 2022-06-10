c5d61dacc10d013116d3cad4705c6ec1137d05ce
sfome <semenilichevsky@gmail.com>
2022-06-11 00:01:45 +0300

    Fix: remove funcs in packaging scripts

--- main.lua
+++ main.lua
@@ -10,12 +10,14 @@ function split(str, sep)
   end
   return tbl
 end
+
 function sh(shell)
-  local handle = io.popen(shell) 
+  local handle = io.popen(shell)
   local result = handle:read("*a")
   handle:close()
   return result
 end
+
 function scandir(dir)
   local i, t, popen = 0, {}, io.popen
   local pfile = popen('ls -a "'..dir..'"')
@@ -26,11 +28,13 @@ function scandir(dir)
   pfile:close()
   return t
 end
+
 function file_exists(file)
   local f = io.open(file)
   if f then f:close() end
   return f ~= nil
 end
+
 function lines_from(file)
   if not file_exists(file) then return {} end
   lines = {}
@@ -52,7 +56,7 @@ end
 
 io.write("\n[ usp ]: ")
 local options = split(io.read("*l"), ',')
-for i,v in pairs(options) do 
+for i,v in pairs(options) do
   print("["..pkgs[tonumber(v)].."]")
-  print(sh("sh packaging/"..pkgs[tonumber(v)])) 
+  print(sh("sh packaging/"..pkgs[tonumber(v)]))
 end
